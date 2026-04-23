67a83ba6148e6049d902244e0276a0a05afc6511
MallikarjunaDiv <bhajarangi05@gmail.com>
2026-04-23 17:35:13 +0530

    fix: railway port binding

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 connectToDb();
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/auth", authRoutes);
